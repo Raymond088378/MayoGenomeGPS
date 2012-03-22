1d6502a3d28af2c0c587258ee46035f9b0efb618
m078940 <m078940@8f329c14-d232-4f17-8684-7ee34322b8dc>
2012-03-22 22:25:04 +0000

    git-svn-id: https://bsisvn.mayo.edu/main/projects/g/Genome_GPS/trunk@596 8f329c14-d232-4f17-8684-7ee34322b8dc

--- annotate_sample.sh
+++ annotate_sample.sh
@@ -12,21 +12,28 @@ else
     script_path=$( cat $tool_info | grep -w '^WHOLEGENOME_PATH' | cut -d '=' -f2 )
     java=$( cat $tool_info | grep -w '^JAVA' | cut -d '=' -f2 )
 	genome_version=$(cat $run_info | grep -w '^GENOMEBUILD' | cut -d '=' -f2)
+	variant_type=$( cat $run_info | grep -w '^VARIANT_TYPE' | cut -d '=' -f2)
     ## jar script to add IGV, pathway, TIssue specificity and Gene Card link
     $java/java -Xmx2g -Xms512m -jar $script_path/exome_annot.jar annotate $tool_info $output_dir/Reports_per_Sample/ $genome_version
 	
     cd $output_dir/Reports_per_Sample/
-    ## sort the INDEL file
-    for i in *.INDEL.*.xls
-    do
-        perl $script_path/sort.variantReport.pl -i $i -o $i.sort -f Start
-        mv $i.sort $i
-    done
-    ## sort the SNP file
-    for i in *.SNV.*.xls
-    do
-        perl $script_path/sort.variantReport.pl -i $i -o $i.sort -f Position
-        mv $i.sort $i
-    done
-    echo `date`
+    if [ $variant_type == "BOTH" -o $variant_type == "INDEL" ]
+	then
+		## sort the INDEL file
+		for i in *.INDEL.*.xls
+		do
+			perl $script_path/sort.variantReport.pl -i $i -o $i.sort -f Start
+			mv $i.sort $i
+		done
+    fi
+	if [ $variant_type == "BOTH" -o $variant_type == "SNV" ]
+	then
+		## sort the SNP file
+		for i in *.SNV.*.xls
+		do
+			perl $script_path/sort.variantReport.pl -i $i -o $i.sort -f Position
+			mv $i.sort $i
+		done
+    fi
+	echo `date`
 fi	
