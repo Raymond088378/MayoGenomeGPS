7e1be1a5b7dbd1996786d5b4da9b29860b116bf6
m078940 <m078940@8f329c14-d232-4f17-8684-7ee34322b8dc>
2012-09-16 21:57:38 +0000

    will work with zipped fastq too
    git-svn-id: https://bsisvn.mayo.edu/main/projects/g/Genome_GPS/trunk@3239 8f329c14-d232-4f17-8684-7ee34322b8dc

--- checkFastqQualityScores.pl
+++ checkFastqQualityScores.pl
@@ -1,45 +1,44 @@
+#!/usr/local/biotools/perl/5.10.0/bin/perl
 use strict ; 
 use warnings;
 
-our $file = $ARGV[0]; 
-our $reads = $ARGV[1];
+our $file = shift @ARGV; 
+our $reads = shift @ARGV;
 my $num_lines=$reads*4;
 our $min = 255 ; 
 our $max = 0 ; 
-
-#   $SIG{'INT'} = sub { print "overall MIN $min / MAX $max\t$file\n"; exit ; };
-open FH, "$file" or die "";
+my $ext = ($file =~ m/([^.]+)$/)[0];
+if ($ext eq "gz")	{open FH, "gunzip -c $file |" or die "can not open the $file : $! \n";}
+else{open FH, "$file" or die "can not open the $file : $! \n";}	
 
 while(<FH>){
     if ($. < $num_lines){
-	if($_ =~ /^\+/)	{
-	    my $line = <FH>; # get the quality line
-	    chomp $line;
-	    #print "$line\n";
-	    my $fred_scores = $line; 
-	    my $converted_fred_scores ; 
-	
-	    my $linemin = 255 ; 
-	    my $linemax = 0 ; 
-	
-	    foreach my $this_score (split(//,$fred_scores)){
-		$converted_fred_scores .= chr(ord($this_score) - 31) ; 
-	
-		my $score = ord($this_score); 
-		if($score > $linemax){ $linemax = $score ; }
-		if($score < $linemin){ $linemin = $score ; }
+		if($_ =~ /^\+/)	{
+			my $line = <FH>; # get the quality line
+			chomp $line;
+			my $fred_scores = $line; 
+			my $converted_fred_scores ; 
 		
-		if($linemax != 0 && $linemax > $max){ $max = $linemax ; }
-		if($linemin != 255 && $linemin < $min){ $min = $linemin ; }
-	
-	    }
-	}
+			my $linemin = 255 ; 
+			my $linemax = 0 ; 
+		
+			foreach my $this_score (split(//,$fred_scores)){
+			$converted_fred_scores .= chr(ord($this_score) - 31) ; 
+		
+			my $score = ord($this_score); 
+			if($score > $linemax){ $linemax = $score ; }
+			if($score < $linemin){ $linemin = $score ; }
+			
+			if($linemax != 0 && $linemax > $max){ $max = $linemax ; }
+				if($linemin != 255 && $linemin < $min){ $min = $linemin ; }
+		
+			}
+		}
     }
     else	{
-	last;
+		last;
     }	
 }
-
 print "$min";
 
 
