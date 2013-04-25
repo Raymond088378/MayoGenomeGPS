50a4ab6df9de42f63e8ff4c880d26adfa67319be
m102772 <m102772@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-04-25 14:38:33 +0000

    Initial import of single sample backfill task script
    git-svn-id: https://bsisvn.mayo.edu/main/projects/g/Genome_GPS/trunk@9278 8f329c14-d232-4f17-8684-7ee34322b8dc

---
+++ backfill_single_sample.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+### Case 1: GATK EXOME ALL_SITES
+### Case 2: GATK EXOME SOME_SITES
+### Case 3: GATK WHOLE_GENOME
+### Case 4: SNV_MIX, EXOME, ALL_SITES
+### Case 5: SNV_MIX, EXOME, SOME_SITES
+### Case 6:	SNV_MIX, WHOLE_GENOME 
+### Case 6: BEAUTY EXOME
+### Case 7: BEAUTY WHOLE_GENOME
+			
+### if [[ "$all_sites" == "YES" ]] $output/$sample.chr${chr}.all.vcf
+###		else $output/$sample.chr${chr}.vcf
+
