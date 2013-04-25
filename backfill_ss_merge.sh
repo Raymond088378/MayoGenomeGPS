b4caeb4ce21ac940b68527fb788b165518089637
m102772 <m102772@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-04-25 14:55:58 +0000

    Initial commit backfill merge (gather) step
    git-svn-id: https://bsisvn.mayo.edu/main/projects/g/Genome_GPS/trunk@9280 8f329c14-d232-4f17-8684-7ee34322b8dc

---
+++ backfill_ss_merge.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+### This should be submitted as single task to create a merged vcf for backfilling
+
+### Case 1: GATK EXOME ALL_SITES
+### Case 2: GATK EXOME SOME_SITES
+### Case 3: GATK WHOLE_GENOME
+### Case 4: SNV_MIX, EXOME, ALL_SITES
+### Case 5: SNV_MIX, EXOME, SOME_SITES
+### Case 6:	SNV_MIX, WHOLE_GENOME 
+### Case 6: BEAUTY EXOME
+### Case 7: BEAUTY WHOLE_GENOME
+			
+### if [[ "$all_sites" == "YES" ]] $output/$sample.chr${chr}.all.vcf
+###		else $output/$sample.chr${chr}.vcf
+
+#!/bin/bash
+
+####
+# backfill_ss_merge.sh
+# creates a merged variant file containing all samples
+# 4/24/2013
+# usage:
+####
+
+function print_usage()
+{
+	
+}
+
+function check_vars()
+{
+	if [[ $1 != $2 ]] 
+	then
+		echo -e "\nExpected $2 entries on the command line, got $1.\n"
+		print_usage
+		exit 1
+	fi		
+}
+
+## exist_or_fail "variable name" variable
+## 		check if variable is set; if not, exit script with error message
+function exist_or_fail ()
+{
+	local message=$1;
+	local found="";
+	shift
+	if [[ "$1" == "" ]] 
+	then 
+		echo "$message is not set."
+		exit 1
+	fi
+}
+
+function load_configuration_or_fail ()
+{
+	local run_info=$1
+	
+	if [[ ! -f $run_info ]]
+	then
+		echo "Failed to find run_info. Exiting."
+		exit 1
+	fi
+	
+	## Load directory names from the run_info and associated configuration files
+	local tool_info=$( grep -w '^TOOL_INFO' $run_info | cut -d '=' -f2 )
+	local script_path=$( grep -w '^WORKFLOW_PATH' $tool_info | cut -d '=' -f2)
+	local memory_info=$( grep -w '^MEMORY_INFO' $run_info | cut -d '=' -f2)
+    local base_output=$( grep -w '^BASE_OUTPUT_DIR' $run_info | cut -d '=' -f2)
+	local run_num=$( grep -w '^OUTPUT_FOLDER' $run_info | cut -d '=' -f2)
+	local javapath=$( grep -w '^JAVA' $tool_info | cut -d '=' -f2)
+    
+    exist_or_fail "$run_info:TOOL_INFO" "$tool_info" 
+    exist_or_fail "$tool_info:WORKFLOW_PATH" "$script_path" 
+    exist_or_fail "$run_info:MEMORY_INFO" "$memory_info"
+    exist_or_fail "$run_info:BASE_OUTPUT_DIR" "$base_output"
+    exist_or_fail "$tool_info:OUTPUT_FOLDER" "$run_num" 
+    exist_or_fail "$tool_info:JAVA" "$javapath"
+	
+	
+         
+}
+
+load_configuration_or_fail
+
+
+
+
+
