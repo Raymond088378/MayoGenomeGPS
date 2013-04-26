bbd2106e79673ee9bfca9109db454be00509e6ae
M102772 <M102772@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-04-26 14:50:01 +0000

    git-svn-id: https://bsisvn.mayo.edu/main/projects/g/Genome_GPS/trunk@9302 8f329c14-d232-4f17-8684-7ee34322b8dc

--- backfill_ss_merge.sh
+++ backfill_ss_merge.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
 
-### This should be submitted as single task to create a merged vcf for backfilling
-
-### Case 1: GATK EXOME ALL_SITES
-### Case 2: GATK EXOME SOME_SITES
-### Case 3: GATK WHOLE_GENOME
-### Case 4: SNV_MIX, EXOME, ALL_SITES
-### Case 5: SNV_MIX, EXOME, SOME_SITES
-### Case 6:	SNV_MIX, WHOLE_GENOME 
-### Case 6: BEAUTY EXOME
-### Case 7: BEAUTY WHOLE_GENOME
-			
-### if [[ "$all_sites" == "YES" ]] $output/$sample.chr${chr}.all.vcf
-###		else $output/$sample.chr${chr}.vcf
-
-#!/bin/bash
-
 ####
 # backfill_ss_merge.sh
 # creates a merged variant file containing all samples
 # 4/24/2013
 # usage:
+# This should be submitted as single task to create a merged vcf for backfilling
+#
 ####
 
 function print_usage()
